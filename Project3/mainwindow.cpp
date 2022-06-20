fccb337c168b78232cdeb6599336290ddc8112b7
RinRainbow <105558966+RinRainbow@users.noreply.github.com>
2022-06-21 00:07:20 +0800

    This is new Project3 rep

---
+++ Project3/mainwindow.cpp
@@ -0,0 +1,218 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <string>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , graphCount(-1)
+{
+    ui->setupUi(this);
+
+    customPlot = ui->drawer;
+
+    QDesktopWidget * deskwidget = QApplication::desktop();
+    QRect deskgeo = deskwidget->screenGeometry();
+    move((deskgeo.width() - width()) / 2, (deskgeo.height() - height()) / 2);
+
+    /*
+    // generate some data:
+    QVector<double> x(999999), y0(999999), y1(999999); // initialize with entries 0..100
+    for (int i=0; i<999999; ++i)
+    {
+      x[i] = i/50.0 - 999; // x goes from -1 to 1
+      y0[i] = x[i]*x[i]; // let's plot a quadratic function
+      y1[i] = qCos(i/10.0); // exponentially decaying cosine
+    }
+    // create graph and assign data to it:
+    customPlot->addGraph();
+    customPlot->graph(0)->setData(x, y0);
+    customPlot->addGraph();
+    customPlot->graph(1)->setData(x, y1);
+    // set the color of the graph
+    customPlot->graph(0)->setPen(QPen(Qt::green));
+    customPlot->graph(1)->setPen(QPen(Qt::blue));
+    */
+
+    // give the axes some labels:
+    customPlot->xAxis->setLabel("x");
+    customPlot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    customPlot->xAxis->setRange(-100, 100);
+    customPlot->yAxis->setRange(-10000, 10000);
+    // set tick count
+    customPlot->xAxis->ticker()->setTickCount(6);
+    customPlot->yAxis->ticker()->setTickCount(6);
+
+    /*
+    // add phase tracer
+    phaseTracer = new QCPItemTracer(customPlot);
+    phaseTracer->setGraph(customPlot->graph(0));
+    phaseTracer->setGraphKey(0.0);
+    phaseTracer->setStyle(QCPItemTracer::tsCrosshair);
+    phaseTracer->setPen(QPen(Qt::red));
+    phaseTracer->setBrush(Qt::red);
+    phaseTracer->setSize(7);
+    connect(customPlot, SIGNAL(mouseMove(QMouseEvent*)), this, SLOT(drawerMouseMoved(QMouseEvent*)));
+
+    // add label for phase tracer
+    phaseTracerText = new QCPItemText(customPlot);
+    phaseTracerText->position->setType(QCPItemPosition::ptAxisRectRatio);
+    phaseTracerText->setFont(QFont(font().family(), 12));
+    phaseTracerText->setPositionAlignment(Qt::AlignLeft);
+    phaseTracerText->position->setCoords(QPointF(0, 0));
+    phaseTracerText->setText("(-, -)");
+    */
+
+    // Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
+    customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+
+    /*
+    // add legend
+    customPlot->setAutoAddPlottableToLegend(true);
+    customPlot->legend->setVisible(true);
+
+    for(int i = 0; i < 2; i++){
+        customPlot->addGraph(customPlot->xAxis, customPlot->yAxis);
+        //customPlot->graph(i)->setName(str.append(ch+i));
+        customPlot->graph(i)->setPen(customPlot->graph(i)->pen());
+        //customPlot->addPlottable(customPlot->graph(i));
+    }
+
+    connect(customPlot, SIGNAL(legendClick(QCPLegend*,QCPAbstractLegendItem*,QMouseEvent*)), this, SLOT(toggleVisibility(QCPLegend*,QCPAbstractLegendItem*,QMouseEvent*)));
+    */
+
+    // update changes
+    customPlot->replot();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::drawerMouseMoved(QMouseEvent *event){
+    QPoint pos = event->pos();
+    double key = customPlot->xAxis->pixelToCoord(pos.x());
+    phaseTracer->setGraphKey(key);
+
+    double x, y;
+    x = qBound(0., 1. + phaseTracer->position->key() / customPlot->xAxis->range().size(), 0.5);
+    y = qBound(0., 1. - phaseTracer->position->value() / customPlot->yAxis->range().size(), 0.9);// value  coord is TopLeft origin
+    phaseTracerText->position->setCoords(x, y);
+    phaseTracerText->setText("(" + QString::number(phaseTracer->position->key()) + ", " + QString::number(phaseTracer->position->value()) + ")");
+
+    ui->drawer->replot();
+}
+
+/*
+void MainWindow::toggleVisibility(){
+    if(!item) return;
+    QCPPlottableLegendItem *plItem = qobject_cast<QCPPlottableLegendItem*>(item);
+    if(!plItem) return;
+
+    plItem->plottable()->setVisible(!plItem->plottable()->visible());
+}
+*/
+
+void MainWindow::on_createFunction_clicked()
+{
+    qDebug() << "Create Function was clicked!!";
+
+    QCheckBox* showMe = new QCheckBox();
+    showMe->setChecked(true);
+    QLabel* label = new QLabel("");  // v X
+    QLineEdit* inputFunction = new QLineEdit("");
+    QPushButton* bye = new QPushButton("🗑");
+    QHBoxLayout* hlayout = new QHBoxLayout;
+    hlayout->addWidget(showMe);
+    hlayout->addWidget(label);
+    hlayout->addWidget(inputFunction);
+    hlayout->addWidget(bye);
+    ui->verticalLayout->addLayout(hlayout);
+
+    customPlot->addGraph();
+    graphCount++;
+
+    checkBox.push_back(showMe);
+    lab.push_back(label);
+    lineEdit.push_back(inputFunction);
+    pushBotton.push_back(bye);
+    hl.push_back(hlayout);
+
+    QVector<double> x(999999), y(999999);
+    for(int i = 0; i < 999999; i++) {
+        x[i] = i/50.0 - 9999;
+        y[i] = x[i] * x[i]; // input: connect to the back_end ^_^
+    }
+    customPlot->graph(graphCount)->setData(x, y);
+    QColor color = QColorDialog::getColor(
+        "#800a64c8",
+        this,
+        tr("choose color"),
+        QColorDialog::ShowAlphaChannel
+    );
+    if (color.isValid()) {
+        color.name();
+        color.name(QColor::HexArgb);
+        int r, g, b, a;
+        color.getRgb(&r, &g, &b, &a);
+    }
+    QPalette palette = label->palette();
+    palette.setColor(label->backgroundRole(), color);
+    palette.setColor(label->foregroundRole(), Qt::black);
+    label->setAutoFillBackground(true);
+    label->setPalette(palette);
+    label->setText(" ");
+    customPlot->graph(graphCount)->setPen(color);
+
+    connect(showMe, SIGNAL(clicked()), this, SLOT(on_showMe_stateChanged()));
+    connect(bye, SIGNAL(pressed()), this, SLOT(on_bye_clicked()));
+
+    customPlot->replot();
+}
+
+
+void MainWindow::on_showMe_stateChanged()
+{
+    qDebug() << "showMe was clicked!!";
+    for(unsigned long i = 0; i < checkBox.size(); i++) {
+        customPlot->graph(i)->setVisible(checkBox[i]->isChecked());
+    }
+    customPlot->replot();
+}
+
+void MainWindow::on_inputFunction_editingFinished()
+{
+    qDebug() << "inputFunction editing finished!!";
+    // addCurve
+}
+
+
+void MainWindow::on_bye_clicked()
+{
+    qDebug() << "Bye was clicked!!";
+    for(unsigned long i = 0; i < pushBotton.size(); i++) {
+        if(pushBotton[i] == sender()) {
+
+            delete checkBox[i];
+            delete lab[i];
+            delete lineEdit[i];
+            delete pushBotton[i];
+            delete hl[i];
+            customPlot->graph(i)->data()->clear();
+            customPlot->removeGraph(i);
+            graphCount--;
+
+            checkBox.erase(checkBox.begin()+i);
+            lab.erase(lab.begin()+i);
+            lineEdit.erase(lineEdit.begin()+i);
+            pushBotton.erase(pushBotton.begin()+i);
+            hl.erase(hl.begin()+i);
+
+            break;
+        }
+    }
+    customPlot->replot();
+}
+
